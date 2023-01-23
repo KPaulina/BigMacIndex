4f608d5c610598f3bc890ff9193e62504870e15f
KPaulina <41211513+KPaulina@users.noreply.github.com>
2023-01-23 18:39:25 +0100

    fix: adding file with data

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 import numpy as np
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))[:-3]
-DATA_DIR = os.path.join(BASE_DIR, 'data')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 
 def countries_full_name_for_top_5(df: pd.DataFrame) -> pd.DataFrame:
@@ -23,7 +23,7 @@ def countries_full_name_for_top_5(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def load_big_mac_data() -> tuple[pd.DataFrame, pd.DataFrame]:
-    df_big_mac = pd.read_excel(os.path.join(DATA_DIR, 'big_mac_data.xlsx'))
+    df_big_mac = pd.read_excel(os.path.join(BASE_DIR, 'big_mac_data.xlsx'))
     df_big_mac = df_big_mac.sort_values(by='dollar_price', ascending=False)
     df_top_five_big_macs = df_big_mac.iloc[:5]
     df_top_five_big_macs = countries_full_name_for_top_5(df_top_five_big_macs)
