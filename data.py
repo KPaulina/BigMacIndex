48f12b11c1a79a3e7b7501649f7df5198757f3f7
KPaulina <41211513+KPaulina@users.noreply.github.com>
2023-01-23 19:16:54 +0100

    fix: fixing data type issue

--- data.py
+++ data.py
@@ -7,6 +7,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def countries_full_name_for_top_5(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Function created to create column that has names of the countries
+    :param df:
+    :return:
+    '''
     condition = [
         df['country_code'] == "VEN",
         df['country_code'] == "CHE",
@@ -22,7 +27,11 @@ def countries_full_name_for_top_5(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def load_big_mac_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+def load_big_mac_data():
+    '''
+    Function created to load data about big mac index from excel
+    :return:
+    '''
     df_big_mac = pd.read_excel(os.path.join(BASE_DIR, 'big_mac_data.xlsx'))
     df_big_mac = df_big_mac.sort_values(by='dollar_price', ascending=False)
     df_top_five_big_macs = df_big_mac.iloc[:5]
